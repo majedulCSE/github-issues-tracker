5b92343cf6219c0781892a6122498883a385543e
majedulCSE <masud2301@gmail.com>
2026-03-08 11:21:07 +0600

    modal section added

--- script/home.js
+++ script/home.js
@@ -21,7 +21,7 @@ searchInput.addEventListener("keyup", (e) => {
 // use enter press for search
 searchInput.addEventListener("keypress", (e) => {
     if(e.key === "Enter"){
-        const searchText = e.target.value;
+        const searchText = e.target.value.trim().toLowerCase();
         fetch(` https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchText}`)
     .then((res) => res.json())
     .then((json) => displayIssues(json.data));
@@ -58,6 +58,17 @@ closedFilterBtn.addEventListener("click", () => {
     displayIssues(closed);
 });
 
+const manageSpiner = (status) => {
+    if(status == true){
+        document.getElementById("spiner").classList.remove("hidden");
+        document.getElementById("container").classList.add("hidden");
+    }
+    else{
+        document.getElementById("container").classList.remove("hidden");
+        document.getElementById("spiner").classList.add("hidden");
+    }
+};
+
 // calculate card
 function calculateCard (){
     issuesCount.innerText = cardContainer.children.length;
@@ -76,6 +87,7 @@ calculateCard();
 
 // issues fetch 
 const loadIssues = () => {
+    //manageSpiner(true);
     fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues')
     .then ((res) => res.json())
     .then ((json) => {
@@ -85,23 +97,6 @@ const loadIssues = () => {
         displayIssues(issues);
     });
 };
-
-
-// {
-//     "id": 46,
-//     "title": "Implement data backup system",
-//     "description": "Set up automated daily backups of database with retention policy and restore procedures.",
-//     "status": "open",
-//     "labels": [
-//         "enhancement"
-//     ],
-//     "priority": "high",
-//     "author": "backup_bruce",
-//     "assignee": "db_admin",
-//     "createdAt": "2024-02-08T09:15:00Z",
-//     "updatedAt": "2024-02-08T09:15:00Z"
-// }
-
 
 const createLevels = (labels) => {
     let labelHtml = "";
@@ -113,13 +108,27 @@ const createLevels = (labels) => {
         }
         else if (label === "help wanted"){
             labelHtml += `
-            <p class="bg-yellow-200 px-3 py-1 text-yellow-600 rounded-4xl"><i class="fa-brands fa-chrome"></i> HELP WANTED</p>
+            <p class="bg-yellow-200 px-3 py-1 text-yellow-600 rounded-4xl gap-1"><i class="fa-brands fa-chrome"></i>HELP WANTED</p>
             `;
         }
         else if (label === "enhancement"){
             labelHtml += `
-            <p class="bg-green-200 px-3 py-1 text-green-600 rounded-4xl">
-            <i class="fa-solid fa-wand-magic-sparkles"></i> ENHANCEMENT
+            <p class="inline-flex items-center gap-1 bg-green-200 px-3 py-1 w-fit text-green-600 rounded-4xl">
+            <i class="fa-solid fa-wand-magic-sparkles"></i>Enhancement
+            </p>
+            `;
+        }
+        else if (label === "documentation"){
+            labelHtml += `
+            <p class="inline-flex items-center gap-1 bg-blue-200 px-3 py-1 text-blue-600 rounded-4xl">
+            <i class="fa-solid fa-file"></i> Documentation
+            </p>
+            `;
+        }
+        else if (label === "good first issue"){
+            labelHtml += `
+            <p class="inline-flex items-center gap-1 bg-pink-200 py-1 px-3 text-pink-600 rounded-4xl">
+            <i class="fa-solid fa-clover"></i> Good First Issue
             </p>
             `;
         }
@@ -127,19 +136,21 @@ const createLevels = (labels) => {
     return labelHtml;
 };
 
-
 // display issues
 const displayIssues = (issues) => {
     const issuesContainer = document.getElementById('card-container');
     issuesContainer.innerHTML = "";
     for (let issue of issues){
         const card = document.createElement("div");
+        card.addEventListener("click", () => {
+            loadingSingleIssu(issue.id);
+        });
         card.classList = `bg-white p-10 shadow rounded-2xl space-y-4 border-t-3 ${issue.status === 'open' ? "border-green-500" : "border-purple-500"}`;
         const labelsHTML = createLevels(issue.labels);
         card.innerHTML = `
                 <div class="flex justify-between items-center">
                   <img src="${issue.status === "open" ? "./assets/Open-Status.png" : "./assets/Closed-Status.png"}" alt="">
-                  <p class=" px-3 py-1 ${issue.priority === "high" ? "bg-red-200 test-red-600" : issue.priority === "medium" ?  "bg-yellow-200 test-yellow-600" : "bg-gray-200 text-gray-600"} rounded-4xl">${issue.priority}</p>
+                  <p class=" px-3 py-1 ${issue.priority === "high" ? "bg-red-200 text-red-600" : issue.priority === "medium" ?  "bg-yellow-200 text-yellow-600" : "bg-gray-200 text-gray-600"} rounded-4xl">${issue.priority}</p>
                 </div>
                 <div class="space-y-1.5">
                   <h3 class="text-xl font-bold text-prim">${issue.title}</h3>
@@ -150,12 +161,58 @@ const displayIssues = (issues) => {
                 </div>
                 <div class="border-t border-gray-300">
                 <div class = "mt-4">
-                  <p class="text-p">${issue.author} <br> ${issue.createdAt} <br>  ${issue.assignee} <br> ${issue.updatedAt} </p>
+                  <p class="text-p">${issue.author ? issue.author : "N/A"} <br> ${issue.createdAt ? issue.createdAt : "N/A"} <br>  ${issue.assignee ? issue.assignee : "N/A"} <br> ${issue.updatedAt ? issue.updatedAt : "N/A"} </p>
                 </div>
         `;
         issuesContainer.appendChild(card);
-    }
+        //manageSpiner(false);
+    };
     calculateCard();
 };
+
+const loadingSingleIssu = (id) => {
+    fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
+    .then((res) => res.json())
+    .then((json) => showModal(json.data));
+};
+
+const showModal = (issue) => {
+    const modal = document.getElementById("issue-modal");
+    const modalContent = document.getElementById("modal-content");
+
+    const labelsHtml = createLevels(issue.labels);
+
+    modalContent.innerHTML = `
+    <div class="max-w-md space-y-4 bg-white">
+        <h3 class="text-xl font-bold text-prim">${issue.title}</h3>
+        <div class="flex items-center gap-6">
+            <p class=" px-3 py-1 text-white ${issue.status === "open" ? "bg-green-500" : "bg-purple-500"} rounded-4xl">${issue.status}</p>
+            <p class="text-p"> •  ${issue.status === "open" ? "Opended" : "Closed"} by ${issue.author ? issue.author : "N/A"}  •  ${issue.createdAt ? issue.createdAt : "N/A"}</p>
+        </div>
+         <div class="flex items-center gap-6">
+            ${labelsHtml}
+        </div>
+        <p class="text-p line-clamp-2">${issue.description}</p>
+        <div class="flex justify-between bg-gray-100 rounded-xl p-3">
+      <div class="space-y-1.5">
+        <p class="text-p">Assignee:</p>
+        <h4 class="font-semibold text-xl">${issue.assignee ? issue.assignee : "N/A"}</h4>
+      </div>
+      <div class="space-y-1.5">
+        <p class="text-p">Priority:</p>
+        <p class=" px-3 py-1 ${issue.priority === "high" ? "bg-red-200 text-red-600" : issue.priority === "medium" ?  "bg-yellow-200 text-yellow-600" : "bg-gray-200 text-gray-600"} rounded-4xl">${issue.priority}</p>
+      </div>
+    </div>
+        <div class="modal-action">
+          <form method="dialog">
+            <!-- if there is a button, it will close the modal -->
+            <button class="btn btn-primary">Close</button>
+          </form>
+        </div>
+    </div>
+    `;
+    modal.showModal();
+}
+
 
 loadIssues();
